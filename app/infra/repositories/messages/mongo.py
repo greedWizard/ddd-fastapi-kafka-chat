78fface0f075147a70f69d210154844e9bede1ad
vyacheslav <skavik46111@gmail.com>
2024-04-01 17:53:44 +0300

    review fixes

--- app/infra/repositories/messages/mongo.py
+++ app/infra/repositories/messages/mongo.py
@@ -19,7 +19,7 @@ class MongoDBChatRepository(BaseChatRepository):
     async def check_chat_exists_by_title(self, title: str) -> bool:
         collection = self._get_chat_collection()
 
-        return await collection.find_one(filter={'title': title})
+        return bool(await collection.find_one(filter={'title': title}))
 
     async def add_chat(self, chat: Chat) -> None:
         collection = self._get_chat_collection()
