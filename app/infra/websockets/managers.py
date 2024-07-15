6709ac7cb1a6159bc899514e30cd58b48c947774
Tamerlan Kabulov <tamerlan.kabulov@emergingtravel.com>
2024-07-15 18:16:22 +0200

    changed ws disconnect to copy list

--- app/infra/websockets/managers.py
+++ app/infra/websockets/managers.py
@@ -1,4 +1,3 @@
-import asyncio
 from abc import (
     ABC,
     abstractmethod,
@@ -38,30 +37,22 @@ class BaseConnectionManager(ABC):
 
 @dataclass
 class ConnectionManager(BaseConnectionManager):
-    lock_map: dict[str, asyncio.Lock] = field(default_factory=dict)
 
     async def accept_connection(self, websocket: WebSocket, key: str):
         await websocket.accept()
-
-        if key not in self.lock_map:
-            self.lock_map[key] = asyncio.Lock()
-
-        async with self.lock_map[key]:
-            # TODO: проверять не находится ли чат в процессе удаления
-            self.connections_map[key].append(websocket)
+        self.connections_map[key].append(websocket)
 
     async def remove_connection(self, websocket: WebSocket, key: str):
-        async with self.lock_map[key]:
-            self.connections_map[key].remove(websocket)
+        self.connections_map[key].remove(websocket)
 
     async def send_all(self, key: str, bytes_: bytes):
         for websocket in self.connections_map[key]:
             await websocket.send_bytes(bytes_)
 
     async def disconnect_all(self, key: str):
-        async with self.lock_map[key]:
-            for websocket in self.connections_map[key]:
-                await websocket.send_json({
-                    'message': 'Chat has been deleted',
-                })
-                await websocket.close()
+        temp_conn_map = self.connections_map[key].copy()
+        for websocket in temp_conn_map:
+            await websocket.send_json({
+                'message': 'Chat has been deleted'
+            })
+            await websocket.close()
